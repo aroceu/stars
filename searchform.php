3a442a10ea6fc0ca7f00f62736614e36572083ba
Arrow Sparrow <dev@aroceu.cloud>
2024-11-12 09:52:21 -0500

    Create searchform.php

---
+++ searchform.php
@@ -0,0 +1,5 @@
+<form method="get" id="search-form-alt" action="'. esc_url(home_url('/')) .'">
+	<label class="hidden" for="s" ></label>
+		<input type="text" name="s" id="s" placeholder="Type here and..">
+		<input type="submit" id="searchsubmit" value="search!" class="textInput"/>
+	</form>
